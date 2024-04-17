46d507aadafca8829dfcab7441b92e76d797a378
Thiago Limonge Santana <156456325+Limonge17@users.noreply.github.com>
2024-04-17 03:06:27 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Entrada de Dados e Declaração de variavéis
+    int Idade;
+    Console.WriteLine("Informe sua idade: ");
+    Idade = int.Parse(Console.ReadLine());
+        //Saída de Dados
+        if (Idade <= 17)
+        {
+    Console.WriteLine("Você é menor de idade!");
+        }
+        else if  (Idade >= 18 && Idade <= 64)
+        {
+    Console.WriteLine("Você é maior de idade, porém não é idoso!");
+        }
+        else if (Idade >= 65)
+        {
+    Console.WriteLine("Você é idoso!");
+        }
+    }
+}
+     
